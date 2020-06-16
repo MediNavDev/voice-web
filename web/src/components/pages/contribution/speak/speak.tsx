957a829cd02c306099673b8c1f9864c409d00a14
Adrian Cuzman <adriancuzman@gmail.com>
2020-06-16 19:57:33 +0300

    Update .gitignore, docker-compose-build.yaml, and 2 more files...

--- web/src/components/pages/contribution/speak/speak.tsx
+++ web/src/components/pages/contribution/speak/speak.tsx
@@ -546,6 +546,21 @@ class SpeakPage extends React.Component<Props, State> {
     });
   };
 
+  private transformSentence = (sentence: any) =>{
+    const text = sentence.text;
+    if (sentence.text.startsWith("{")){
+      console.log("Parsing text sentence: "+text)
+      const obj = JSON.parse(text);
+      const answers = obj['corr_answ'];
+      const questionText = obj['q_text'].replace("*","");
+      let allAnswersText = "";
+      answers.forEach( (a:String) => { allAnswersText = allAnswersText + a.trim() + ", ";})
+      allAnswersText = allAnswersText.substr(0, allAnswersText.length - 2);
+      return questionText + " " + allAnswersText;
+    }
+    return sentence.text;
+  };
+
   render() {
     const { getString, user } = this.props;
     const {
@@ -698,7 +713,7 @@ class SpeakPage extends React.Component<Props, State> {
                 ? null
                 : clips[recordingIndex].sentence.id,
           }}
-          sentences={clips.map(({ sentence }) => sentence && sentence.text)}
+          sentences={clips.map(({ sentence }) => sentence && this.transformSentence(sentence))}
           shortcuts={[
             {
               key: 'shortcut-record-toggle',
